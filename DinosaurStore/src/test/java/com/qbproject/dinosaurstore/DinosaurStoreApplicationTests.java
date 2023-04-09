b675f259cfb7506e9fdb5e5fcbd3cee22fb1a4a6
ArtsiomChekh <qbartem@gmail.com>
2023-04-09 22:24:39 +0300

    Initial commit

---
+++ DinosaurStore/src/test/java/com/qbproject/dinosaurstore/DinosaurStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qbproject.dinosaurstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinosaurStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
